b932ec163d014ec607a834b783b92fa7fc0a83f8
Kirill Saidov <kirillsaidovd@yahoo.com>
2023-11-01 15:19:43 +0600

    update

--- sockets/unix_client.c
+++ sockets/unix_client.c
@@ -1,5 +1,3 @@
-// Client side C/C++ program to demonstrate Socket
-// programming
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,42 +6,45 @@
 
 #define PORT 8080
 
-int main(int argc, char const* argv[])
-{
-	int status, valread, client_fd;
-	struct sockaddr_in serv_addr;
-	char* hello = "Hello from client";
-	char buffer[1024] = { 0 };
-	if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		printf("\n Socket creation error \n");
-		return -1;
-	}
+int main(int argc, char const* argv[]) {    
+    char* hello = "Hello from client";
+    char buffer[1024] = { 0 };
 
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(PORT);
+    // open socket
+    int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_fd < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
 
-	// Convert IPv4 and IPv6 addresses from text to binary
-	// form
-	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
-		<= 0) {
-		printf(
-			"\nInvalid address/ Address not supported \n");
-		return -1;
-	}
+    // setup connection address
+    struct sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
 
-	if ((status
-		= connect(client_fd, (struct sockaddr*)&serv_addr,
-				sizeof(serv_addr)))
-		< 0) {
-		printf("\nConnection Failed \n");
-		return -1;
-	}
-	send(client_fd, hello, strlen(hello), 0);
-	printf("Hello message sent\n");
-	valread = read(client_fd, buffer, 1024 - 1); // subtract 1 for the null terminator at the end
-	printf("%s\n", buffer);
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
 
-	// closing the connected socket
-	close(client_fd);
-	return 0;
+    // connect to server
+    int status = connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))
+    if (status < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    // send to server
+    send(client_fd, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+
+    // read from server
+    int = valread = read(client_fd, buffer, 1024 - 1); // subtract 1 for the null terminator at the end
+    printf("%s\n", buffer);
+
+    // closing the connected socket
+    close(client_fd);
+
+    return 0;
 }
--- sockets/unix_server.c
+++ sockets/unix_server.c
@@ -26,6 +26,7 @@ int main(int argc, char const* argv[]) {
         exit(EXIT_FAILURE);
     }
 
+    // create connection address
     struct sockaddr_in address;
     socklen_t addrlen = sizeof(address);
     address.sin_family = AF_INET;
@@ -38,19 +39,24 @@ int main(int argc, char const* argv[]) {
         exit(EXIT_FAILURE);
     }
 
+    // listen to available connections
     if (listen(server_fd, 3) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
+    // accept incoming connection
     int new_socket;
     if ((new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen)) < 0) {
         perror("accept");
         exit(EXIT_FAILURE);
     }
+
+    // read from client
     ssize_t valread = read(new_socket, buffer, 1024 - 1); // subtract 1 for the null // terminator at the end
     printf("%s\n", buffer); 
 
+    // send to client
     send(new_socket, msg_hello, strlen(msg_hello), 0);
     printf("Hello message sent\n");
 
@@ -59,5 +65,6 @@ int main(int argc, char const* argv[]) {
 
     // closing the listening socket
     close(server_fd);
+    
     return 0;
 }
